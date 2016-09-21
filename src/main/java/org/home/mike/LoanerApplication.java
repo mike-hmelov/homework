ad27e2b20bb71e29749e425349a38b0f1ddeb9f0
mike.hmelov <mike.hmelov@gmail.com>
2016-09-21 23:20:03 +0300

    initial

---
+++ src/main/java/org/home/mike/LoanerApplication.java
@@ -0,0 +1,12 @@
+package org.home.mike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanerApplication.class, args);
+	}
+}
